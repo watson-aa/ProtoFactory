0d2326a62ca9da3f3a26d41fcb919aacd8d90607
aaron <aaron@Aarons-MacBook.aholdusa.com>
2018-08-16 16:27:01 -0400

    fun with streams

--- src/ProtoFactory/FileReader/OrderFileReader.java
+++ src/ProtoFactory/FileReader/OrderFileReader.java
@@ -1,4 +1,27 @@
 package ProtoFactory.FileReader;
 
-public class OrderFileReader {
+import ProtoFactory.WidgetSpec;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
+
+public class OrderFileReader extends FileReaderSuper {
+    public WidgetSpec[] parse(String filename, WidgetSpec[] widgetSpecs) {
+        ArrayList<WidgetSpec> oWidgetSpecs = new ArrayList<>();
+
+        try {
+            for (String id : read(filename)) {
+                Optional<WidgetSpec> optWS = Arrays.stream(widgetSpecs).filter(widgetSpec -> id.equals(widgetSpec.getId())).findFirst();
+                if (((Optional) optWS).isPresent()) {
+                    oWidgetSpecs.add(optWS.get());
+                }
+            }
+
+            return (WidgetSpec[]) oWidgetSpecs.toArray(new WidgetSpec[oWidgetSpecs.size()]);
+        } catch (IOException e) {
+            return new WidgetSpec[0];
+        }
+    }
 }
